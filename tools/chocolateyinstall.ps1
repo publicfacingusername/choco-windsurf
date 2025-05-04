82df9ff39f8d59fc838041e64f87597df313c5e9
publicfacingusername <github@mail4j.com>
2025-05-04 15:55:48 -0400

    Add files for Windsurf v1.98.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://windsurf-stable.codeiumdata.com/win32-x64-user/stable/b21eedafd0e27ae3d0a6e454346f7b02178f0949/WindsurfUserSetup-x64-1.7.1.exe'
+$url        = 'https://windsurf-stable.codeiumdata.com/win32-x64-user/stable/71eeb18eeed7897bea630fcaba7d37c49c78b05e/WindsurfUserSetup-x64-1.7.3.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   softwareName  = 'Windsurf*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = 'FFB462C95818CC34503EB42AB094479501008AD6474CB2D5C6D2D757CB7CA661'
+  checksum      = 'FB8649E59605153514D0FB4ECEA6B85F3BBF4D6A9D0975551AE352EEABEB5306'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   silentArgs    = "/VERYSILENT" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
