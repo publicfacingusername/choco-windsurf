e4e3e0a3963ff02a9da0ecfb8e3c382784129fb9
publicfacingusername <github@mail4j.com>
2025-05-13 01:37:25 -0400

    Fix checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   softwareName  = 'Windsurf*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = 'FB8649E59605153514D0FB4ECEA6B85F3BBF4D6A9D0975551AE352EEABEB5306'
+  checksum      = 'FFB462C95818CC34503EB42AB094479501008AD6474CB2D5C6D2D757CB7CA661'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   silentArgs    = "/VERYSILENT" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
